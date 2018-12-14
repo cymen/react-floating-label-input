b314b596883ee5f281b876522bafdbb48cd6fcce
Cymen Vig <cymenvig@gmail.com>
2018-12-13 19:33:52 -0500

    update examples to match v4 props

--- stories/index.js
+++ stories/index.js
@@ -12,7 +12,7 @@ storiesOf('FloatingLabelInput', module)
     () => (
       <div style={{ margin: 10 }}>
         <div style={{ border: '1px dashed #ddd', padding: '1em', margin: '10px 0' }}>
-          <div>Default font size of page and default label shrink of 70%:</div>
+          <div>Default:</div>
           <FloatingLabelInput
             id="example-1"
             label="label"
@@ -20,7 +20,7 @@ storiesOf('FloatingLabelInput', module)
           />
         </div>
         <div style={{ border: '1px dashed #ddd', padding: '1em', margin: '10px 0' }}>
-          <div>Font size of 20px, default label shrink size of 70% (20 * 0.7 = 14px) and no placeholder:</div>
+          <div>Font size of 20px and placeholder:</div>
           <div style={{ fontSize: 20 }}>
             <FloatingLabelInput
               id="example-2"
@@ -31,25 +31,23 @@ storiesOf('FloatingLabelInput', module)
           </div>
         </div>
         <div style={{ border: '1px dashed #ddd', padding: '1em', margin: '10px 0' }}>
-          <div>Font size of 64px, label shrink size of 20% and no placeholder:</div>
+          <div>Font size of 64px and no placeholder:</div>
           <div style={{ fontSize: 64 }}>
             <FloatingLabelInput
               id="example-3"
               label="label"
               onChange={action('onChange')}
-              shrink={20}
             />
           </div>
         </div>
         <div style={{ border: '1px dashed #ddd', padding: '1em', margin: '10px 0' }}>
-          <div>Font size of 64px and label shrink size of 90%:</div>
+          <div>Font size of 64px and placeholder:</div>
           <div style={{ fontSize: 64 }}>
             <FloatingLabelInput
               id="example-3"
               label="label"
               onChange={action('onChange')}
               placeholder="placeholder"
-              shrink={90}
             />
           </div>
         </div>
